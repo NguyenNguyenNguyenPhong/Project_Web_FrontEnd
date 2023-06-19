1a8c728c117ff7fbf44b25e88238de259850a2f9
thiencutovc <thien.lm204608@sis.hust.edu.vn>
2023-06-19 21:38:26 +0700

    chinh giao dien

--- UI_Hard_Code/src/views/blog/BlogContainer.js
+++ UI_Hard_Code/src/views/blog/BlogContainer.js
@@ -27,26 +27,48 @@ const BlogContainer = () => {
     console.log(data)
 
     return (
+
         <div>
             <Navigation>
             </Navigation>
+
+            <h1 style={{ margin: "20px" }}>Các bài đăng nổi bật</h1>
             <div className="container">
-                <h1 style={{ margin: "20px" }}>Các bài đăng nổi bật</h1>
                 <ul className="blog-list"> {/* Apply the "blog-list" class to the <ul> element */}
                     {data.map((blog) => (
                         <li key={blog.id} className="blog-list-item"> {/* Apply the "blog-list-item" class to each <li> element */}
                             <img className="image" src={blog.image} alt={blog.name} />
-                            
+
                             <div className="content">
                                 <div className="category"><strong>Topic:</strong>{blog.category}</div>
-                                 <a href={blog.content} target="_blank" rel="noopener noreferrer"> 
-                                <div className="name">{blog.name}</div>
-                                 </a>
+                                <a href={blog.content} target="_blank" rel="noopener noreferrer">
+                                    <div className="name">{blog.name}</div>
+                                </a>
                             </div>
-                           
+
                         </li>
                     ))}
                 </ul>
+                <div className="blog-list-recommendation">
+                    <h3>CHỦ ĐỀ NỔI BẬT KHÁC</h3>
+                    <p className='recommend-topic'>
+                        <strong>Web development</strong>
+                    </p>
+                    <p className='recommend-topic'>
+                        <strong>Neural Network</strong>
+                    </p>
+                    <p className='recommend-topic'>
+                        <strong>Computational Optimization</strong>
+                    </p>
+
+                    <a className="recommend-image">
+                        <img className="image" src={"https://files.fullstack.edu.vn/f8-prod/banners/26/63dc61f2a061e.png"} />
+                    </a>
+
+                    <a className="recommend-image">
+                        <img className="image" src={"https://files.fullstack.edu.vn/f8-prod/banners/32/6421144f7b504.png"} />
+                    </a>
+                </div>
             </div>
             <FooterContainer>
 
